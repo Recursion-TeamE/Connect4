8deb4b7e6a85a775c4d0cda7e86f70ebed57b3a0
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2021-09-24 07:02:52 +0900

    Created an animation.

--- connect-four/src/App.vue
+++ connect-four/src/App.vue
@@ -2,17 +2,20 @@
   <div id="app">
     <h1>Connect 4</h1>
     <p>Click columns to play</p>
+    <Ball :x="200" :y="165" />
     <Controller />
   </div>
 </template>
 
 <script>
 import Controller from "./components/Controller.vue";
+import Ball from "./components/Ball.vue";
 
 export default {
   name: "app",
   components: {
     Controller,
+    Ball,
   },
 };
 </script>
