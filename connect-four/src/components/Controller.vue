25f2f9f5626366f74cc5667459ab5c78d63bb4eb
Acha0203 <74553433+Acha0203@users.noreply.github.com>
2021-09-24 06:43:28 +0900

    Remove the animation.

--- connect-four/src/components/Controller.vue
+++ connect-four/src/components/Controller.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <p>--------- Controller Component ----------</p>
-    <Ball :x="200" :y="165" />
     <Board />
 
     <div class="">
@@ -14,12 +13,10 @@
 
 <script>
 import Board from "./Board.vue";
-import Ball from "./Ball.vue";
 
 export default {
   components: {
     Board,
-    Ball,
   },
   data() {
     return {};
